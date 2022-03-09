9ddef5e3dbfb008b51ffd56f8cf9b3ad81d57b68
arsenewe <enggar@polinema.ac.id>
2022-03-09 19:44:19 +0700

    tambah template kuis1F

---
+++ GameMain.java
@@ -0,0 +1,25 @@
+public class GameMain {
+    public static void main(String[] args) {
+        System.out.println("=========PACMAN=========");
+        System.out.println("\nTentukan jumlah Pacman: ");
+        
+        
+        System.out.println("\nTentukan jumlah pil untuk setiap Pacman: ");
+        
+        
+        System.out.println("\n\n=========KUMAN=========");
+        System.out.println("\nTentukan jumlah Kuman: ");
+        
+        
+        System.out.println("\n\n======GAME PACMAN VS KUMAN=====");
+        
+        System.out.println("\nPilih Pacman yang kamu mainkan!");
+        System.out.println("Pilih Pacman ke: ");
+        
+        System.out.println("\nTentukan jumlah pil yang dimakan oleh Pacman ke-"+ p+"!");
+       
+        System.out.println("\nTentukan kuman yang kamu mainkan!");
+        System.out.println("Pilih kuman ke:");
+        
+    }
+}
---
+++ Pacman.java
@@ -0,0 +1,15 @@
+public class Pacman {
+    int pil;
+    
+    Pacman (int pil){
+        
+        System.out.println("Energi default sebesar: " + this.pil + " diberikan kepada ");
+    }
+    
+    
+    
+    void makanPil(int n){
+        
+        System.out.println("Energi yang diperoleh Pacman: " + pil);
+    }
+}
